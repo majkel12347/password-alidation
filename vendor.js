d2a30bb800d8d87962d6811107d0a3048e2953e0
Marcin <majkel12347@o2.pl>
2022-10-07 09:03:14 +0200

    finish js file

--- vendor.js
+++ vendor.js
@@ -1,1 +1,20 @@
 
+const passwordInput = document.getElementById("password");
+const submitBtn = document.getElementById("sub");
+
+const re = /^[A-Za-z]\w{7,14}$/;
+
+const checkPass = (inputTxt) => {
+  if (inputTxt.value.match(re)) {
+    console.log("good");
+    return true;
+  } else {
+    console.log("false");
+    passwordInput.style.border = "2px solid orangered";
+    return false;
+  }
+};
+
+submitBtn.addEventListener("click", (e) => {
+  checkPass(passwordInput);
+});
