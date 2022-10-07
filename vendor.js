ccfe7edd3ffd250eccaffb06c01cecda6db4101b
Marcin <majkel12347@o2.pl>
2022-10-07 15:00:27 +0200

    js clean up

--- vendor.js
+++ vendor.js
@@ -6,10 +6,8 @@ const re = /^[A-Za-z]\w{7,14}$/;
 
 const checkPass = (inputTxt) => {
   if (inputTxt.value.match(re)) {
-    console.log("good");
     return true;
   } else {
-    console.log("false");
     passwordInput.style.border = "2px solid orangered";
     return false;
   }
